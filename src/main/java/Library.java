ca47709ef12df30acb8820be19ea3a26d9f72d7b
Ross Mckay <ross.mckay1975@gmail.com>
2019-10-08 23:20:05 +0100

    library_has_capacity

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class Library {
 
+    private ArrayList<Book> stock;
+    private int capacity;
 
+    public Library(int capacity) {
+        this.stock = new ArrayList<Book>();
+        this.capacity = capacity;
+    }
 
+    public int bookCount() {
+        return this.stock.size();
+    }
+
+    public void addToStock(Book book){
+        this.stock.add(book);
+
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
 
     private Library library;
@@ -8,11 +10,24 @@ public class LibraryTest {
 
     @Before
     public void before(){
-        library = new Library()
+        library = new Library(100);
+//        book = new Book();
     }
 
     @Test
     public void checkLibraryEmpty() {
-        assertEquals(0, get.)
+        assertEquals(0, library.bookCount());
+    }
+
+//    @Test
+//    public void addBookToStock(){
+//        library.addToStock(book);
+//        assertEquals(1, library.bookCount());
+//    }
+
+    @Test
+    public void checkLibraryHasCapacity(){
+        assertEquals(100, library.getCapacity());
+
     }
 }
